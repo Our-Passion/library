6c600939d7cedaeb482dee4555e537ef79b7ec20
mhmadwrekat <mhmmadwreekkat@gmail.com>
2021-08-13 13:01:33 +0300

    add design header and footer

---
+++ js/author.js
@@ -0,0 +1,60 @@
+
+'use strict' ;
+/*
+const imgOne = document.getElementById('author1') ;
+
+let book = [
+  'The Murder of Roger Ackroyd'
+  ,
+  'Murder at the Vicarage'
+] ;
+let puplish = [
+  2006,2020
+] ;
+
+function author (name , book , puplis)
+{
+  this.name = name ;
+  this.book = book ;
+  this.puplis = puplis ;
+
+  author.arr.push(this) ;
+}
+author.arr = []  ;
+
+console.log(author.arr) ;
+
+*/
+
+
+
+
+
+
+/*
+imgOne.addEventListener('click' , Func) ;
+
+function Func (event)
+{
+    let p = document.createElement('p');
+    imgOne.appendChild(p) ;
+    p.textContent = 'Books : '
+    + author.arr[i].book + ' Puplish in '
+    + author .arr[i].puplish ;
+}
+Func() ;
+*/
+/*
+book
+- The Murder of Roger Ackroyd
+- Murder at the Vicarage
+
+puplish
+2006
+2006
+*/
+
+
+
+
+
