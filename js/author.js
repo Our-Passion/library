c1345a268aa76b93558256cb321154d7bd3367aa
mhmadwrekat <mhmmadwreekkat@gmail.com>
2021-08-13 13:36:26 +0300

    structure header & footer

---
+++ js/author.js
@@ -0,0 +1,95 @@
+/*
+'use strict' ;
+
+const imgOne = document.getElementById('author1') ;
+
+let book = [
+  'The Murder of Roger Ackroyd'
+  ,
+  'Murder at the Vicarage'
+] ;
+let puplish = [
+  2006,2020
+] ;
+
+function author (name , book , puplish , img)
+{
+  this.name = name ;
+  this.book = book ;
+  this.puplis = puplis ;
+  this.img = img ;
+  author.arr.push(this) ;
+}
+author.arr = [] ;
+*/
+/*
+
+let srcimg = [
+  'wine-glass.jpg','water-can.jpg','unicorn.jpg','tauntaun.jpg','sweep.png','shark.jpg','scissors.jpg',
+  'pet-sweep.jpg','pen.jpg','dragon.jpg','dog-duck.jpg','cthulhu.jpg','chair.jpg',
+  'bubblegum.jpg','breakfast.jpg','boots.jpg','bathroom.jpg','banana.jpg','bag.jpg'
+] ;
+const section = document.getElementById('pic') ;
+const list = document.getElementById('button') ;
+
+let picOne = document.getElementById('picOne') ;
+let picTwo = document.getElementById('picTwo') ;
+let picThree = document.getElementById('picThree') ;
+let counterClick = 25 ;
+let count = 0 ;
+let prevImg =[] ;
+
+let one = 0 ;
+let two = 0 ;
+let three = 0 ;
+
+function stand (name , picSrc , show = 0 , vote = 0)
+{
+  this.name = name ;
+  this.pic = picSrc ;
+  this.see = show ;
+  this.votes = vote ;
+
+  stand.arr.push(this) ;
+}
+
+stand.arr = [] ;
+    let info = JSON.parse(localStorage.info) ;
+    for(let i = 0 ; i < info.length ; i++)
+    {
+      new stand ( srcimg[i].split('.')[0] , srcimg[i] , info[i].see, info[i].votes ) ;
+    }
+  } else {
+    for ( let i = 0 ; i < srcimg.length ; i++){
+      new stand ( srcimg[i].split('.')[0] , srcimg[i] ) ;
+    }
+*/
+
+
+
+
+
+
+
+/*
+imgOne.addEventListener('click' , Func) ;
+
+function Func (event)
+{
+    let p = document.createElement('p');
+    imgOne.appendChild(p) ;
+    p.textContent = 'Books : '
+    + author.arr[i].book + ' Puplish in '
+    + author .arr[i].puplish ;
+}
+Func() ;
+*/
+/*
+book
+- The Murder of Roger Ackroyd
+- Murder at the Vicarage
+
+puplish
+2006
+2006
+*/
