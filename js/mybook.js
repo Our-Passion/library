36759e005b42d1143bbd1f0c3c586a7aa9e7af3b
maramelayyan <maramelayyan3@gmail.com>
2021-08-17 10:33:45 +0300

    finish script

--- js/mybook.js
+++ js/mybook.js
@@ -2,47 +2,70 @@ const form = document.querySelector('#suggestForm');
 const input = document.querySelector('input');
 const main = document.querySelector('.main');
 const ul = document.querySelector('#suggestedList');
-
+let suggestionarray = localStorage.suggestionarray ? JSON.parse(localStorage.suggestionarray) : [];
 
 function createLi() {
-  const li = document.createElement('li');
-  const span = document.createElement('span');
-  span.textContent = input.value;
-  const label = document.createElement('label');
-  label.textContent = 'available';
-  const checkbox = document.createElement('input');
-  checkbox.type = 'checkbox';
-  const editBtn = document.createElement('button');
-  editBtn.textContent = 'edit';
-  const removeBtn = document.createElement('button');
-  removeBtn.textContent = 'remove';
+ 
+  //return li;
+  ul.innerHTML = '';
 
-  li.appendChild(span);
-  li.appendChild(label);
-  label.appendChild(checkbox);
-  li.appendChild(editBtn);
-  li.appendChild(removeBtn);
-
-  return li;
+  for(let i=0; i< suggestionarray.length; i++){
+    const li = document.createElement('li');
+    const span = document.createElement('span');
+    span.textContent = suggestionarray[i];
+    const label = document.createElement('label');
+    label.textContent = 'available';
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    const editBtn = document.createElement('button');
+    editBtn.textContent = 'edit';
+    const removeBtn = document.createElement('button');
+    removeBtn.textContent = 'remove';
+  
+    li.appendChild(span);
+    li.appendChild(label);
+    label.appendChild(checkbox);
+    li.appendChild(editBtn);
+    li.appendChild(removeBtn);
+    ul.appendChild(li)
+  
+  }
 }
+
+createLi()
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
 
-  const li = createLi();
+ // const li = createLi();
 
-  if(input.value === '') {
-    alert('Enter the name please!!!');
+  if (input.value === '') {
   } else {
-    ul.appendChild(li);
+    //ul.appendChild(li);
+    
+    suggestionarray = JSON.parse(localStorage.getItem("suggestionarray"))
+    if (suggestionarray) {
+      console.log(suggestionarray);
+      suggestionarray.push(input.value)
+      localStorage.setItem("suggestionarray", JSON.stringify(suggestionarray));
+    }
+    else {
+      suggestionarray = []
+      suggestionarray.push(input.value)
+      localStorage.setItem("suggestionarray", JSON.stringify(suggestionarray));
+
+    }
+    createLi()
   }
-}); 
+});
+
+
 
 ul.addEventListener('change', (event) => {
   const checkbox = event.target;
   const checked = checkbox.checked;
   const li = checkbox.parentNode.parentNode;
-  if(checked) {
+  if (checked) {
     li.className = 'checkedd';
   } else {
     li.className = '';
@@ -50,13 +73,13 @@ ul.addEventListener('change', (event) => {
 });
 
 ul.addEventListener('click', (event) => {
-  if(event.target.tagName === 'BUTTON') {
+  if (event.target.tagName === 'BUTTON') {
     const button = event.target;
     const li = button.parentNode;
     const ul = li.parentNode;
-    if(button.textContent === 'remove') {
+    if (button.textContent === 'remove') {
       ul.removeChild(li);
-    } else if(button.textContent === 'edit') {
+    } else if (button.textContent === 'edit') {
       const span = li.firstElementChild;
       const input = document.createElement('input');
       input.type = 'text';
@@ -64,7 +87,7 @@ ul.addEventListener('click', (event) => {
       li.insertBefore(input, span);
       li.removeChild(span);
       button.textContent = 'save';
-    } else if(button.textContent === 'save') {
+    } else if (button.textContent === 'save') {
       const input = li.firstElementChild;
       const span = document.createElement('span');
       span.textContent = input.value;
@@ -90,20 +113,20 @@ filterCheckbox.addEventListener('change', (event) => {
   const isChecked = event.target.checked;
   const lis = ul.children;
 
-  if(isChecked) {
-    for(let i = 0; i < lis.length; i++) {
+  if (isChecked) {
+    for (let i = 0; i < lis.length; i++) {
       var li = lis[i];
 
-      if(li.className === 'checkedd') {
+      if (li.className === 'checkedd') {
         li.style.display = '';
       } else {
         li.style.display = 'none';
       }
     }
   } else {
-    for(let i = 0; i < lis.length; i++) {
+    for (let i = 0; i < lis.length; i++) {
       var li = lis[i];
       li.style.display = '';
     }
   }
-})
+});
